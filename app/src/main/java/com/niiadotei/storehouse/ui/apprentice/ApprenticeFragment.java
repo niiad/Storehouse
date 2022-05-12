48970924d780a00dfb5fa833c0f0d07e1a8c1954
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-05-12 21:43:51 +0000

    clean up

--- app/src/main/java/com/niiadotei/storehouse/ui/apprentice/ApprenticeFragment.java
+++ app/src/main/java/com/niiadotei/storehouse/ui/apprentice/ApprenticeFragment.java
@@ -1,11 +1,7 @@
 package com.niiadotei.storehouse.ui.apprentice;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,8 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.niiadotei.storehouse.DrawerActivity;
-import com.niiadotei.storehouse.R;
 import com.niiadotei.storehouse.databinding.FragmentApprenticeBinding;
 
 public class ApprenticeFragment extends Fragment {
@@ -38,21 +32,6 @@ public class ApprenticeFragment extends Fragment {
         final TextView textView = binding.textApprentice;
         apprenticeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
-    }
-
-    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.apprentice_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-
-        MenuItem dashboard = menu.findItem(R.id.drawer_menu);
-
-        dashboard.setOnMenuItemClickListener(menuItem -> {
-            Intent intent = new Intent(getActivity(), DrawerActivity.class);
-            requireActivity().startActivity(intent);
-
-
-           return false;
-        });
     }
 
     @Override
